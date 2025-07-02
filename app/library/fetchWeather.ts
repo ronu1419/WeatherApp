8cc3820ed03e6e90c7a23a2e078b3fe164a641a0
ronu1419 <garvitpr1997@gmail.com>
2025-07-03 03:16:36 +0530

    Logic written to fetch the data from the api.

---
+++ app/library/fetchWeather.ts
@@ -0,0 +1,25 @@
+import { Coordinates } from "@/types";
+
+export async function fetchCoordinates(city: string) {
+  const response = await fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=0361cbb9a191b573c8693c32276a0672`
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch the data !");
+  }
+
+  return response.json();
+}
+
+export async function fetchWeather(coordinates: Coordinates) {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=0361cbb9a191b573c8693c32276a0672`
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch the data !");
+  }
+
+  return response.json();
+}
